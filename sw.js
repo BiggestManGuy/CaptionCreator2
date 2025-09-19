5e832712db5b08d004aa6e47cf3a63e73f2ba88c
BiggestManGuy <featheredbeast25@gmail.com>
2025-09-19 01:41:33 -0400

    Updates

--- sw.js
+++ sw.js
@@ -61,7 +61,7 @@ var __wpo = {
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "9/19/2025, 1:38:37 AM",
+  "version": "9/19/2025, 1:41:31 AM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
